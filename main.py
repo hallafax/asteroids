cc1e426f1e4ba1d698049bd5fc8adf0989f6c739
hallafax <vswaze@gmail.com>
2025-04-22 02:36:58 -0400

    added clock update

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        #iterate thru the event cue looking for events
         for event in pygame.event.get():
+            #if quit event happens return out of main()
             if event.type == pygame.QUIT:
                 return
             
+        #color the screen black    
         screen.fill("black")
-
+        #update display
         pygame.display.flip()
+
+        #update clock
+        dt = clock.tick() / 1000 
+        clock.tick(60)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
